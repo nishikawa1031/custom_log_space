5a60b409768cc383fe2ce89f30924f32cfc85a90
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-09-12 08:13:13 +0900

    bundle gem custom_log_space

---
+++ lib/custom_log_space.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "custom_log_space/version"
+
+module CustomLogSpace
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/custom_log_space/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CustomLogSpace
+  VERSION = "0.1.0"
+end
