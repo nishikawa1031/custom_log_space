e3b004a51b0a661bca27c879212a00337f61df6c
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-09-14 08:16:08 +0900

    change path

--- lib/custom_log_space/log_writer.rb
+++ lib/custom_log_space/log_writer.rb
@@ -1,13 +1,9 @@
-# frozen_string_literal: true
-
 module CustomLogSpace
-  # The LogWriter module provides methods for writing log messages to custom log files.
-  # It allows the creation of log directories, handling file errors, and appending messages to log files.
   module LogWriter
     private
 
     def write_to_custom_log(message)
-      directory_path = controller_log_directory
+      directory_path = log_directory_based_on_format
       FileUtils.mkdir_p(directory_path) unless Dir.exist?(directory_path)
       custom_log_path = custom_log_file_path(directory_path)
 
@@ -19,26 +15,17 @@ module CustomLogSpace
       handle_file_error(e)
     end
 
-    def handle_file_error(error)
-      error_prefix = error.is_a?(SystemCallError) ? "Error" : "IO Error"
-      puts "#{error_prefix}: #{error.message}"
-    end
-
     def custom_log_file_path(directory_path)
-      action_name = Thread.current[:current_action]
-      log_file_name = "#{action_name}.log"
-      File.join(directory_path, log_file_name)
+      time = Time.now.strftime("%H:%M")
+      "#{directory_path}/#{time}.log"
     end
 
-    def custom_log_directory
-      today = Time.now.strftime("%Y%m%d")
-      time = Time.now.strftime("%H%M")
-      File.join(Rails.root, "log", "custom_log_space", today, time)
-    end
-
-    def controller_log_directory
+    def log_directory_based_on_format
       controller_name = Thread.current[:current_controller].underscore
-      File.join(custom_log_directory, controller_name)
+      action_name = Thread.current[:current_action]
+      date = Time.now.strftime("%Y-%m-%d")
+
+      File.join(Rails.root, "log", "custom_log_space", controller_name, action_name, date)
     end
   end
 end
