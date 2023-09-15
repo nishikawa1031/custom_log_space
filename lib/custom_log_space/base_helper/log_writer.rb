4a6bdfd0549488bfb64b45be9500984cf0e54882
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-09-15 22:29:08 +0900

    [refactor]

--- lib/custom_log_space/base_helper/log_writer.rb
+++ lib/custom_log_space/base_helper/log_writer.rb
@@ -6,10 +6,15 @@ module CustomLogSpace
   module LogWriter
     private
 
-    def log_message(message)
-      current_controller = Thread.current[:current_controller]
-      current_action = Thread.current[:current_action]
+    def current_controller
+      Thread.current[:current_controller]
+    end
 
+    def current_action
+      Thread.current[:current_action]
+    end
+
+    def log_message(message)
       return unless current_controller && current_action
 
       write_to_custom_log(message) do |file|
@@ -20,88 +25,55 @@ module CustomLogSpace
     end
 
     def cleanup_old_directories
-      return unless Dir.exist?(action_directory)
+      return unless Dir.exist?(base_directory_path)
 
-      # If there are more than 3 date-directories, remove the oldest ones
+      # If there are more than 2 date-directories, remove the oldest ones
       remove_oldest_directory while all_directories.size > 2
     end
 
     def all_directories
-      @all_directories ||= Dir.entries(action_directory).select do |entry|
-        File.directory?(File.join(action_directory, entry)) && entry !~ /^\./
+      @all_directories ||= Dir.entries(base_directory_path).select do |entry|
+        File.directory?(File.join(base_directory_path, entry)) && entry !~ /^\./
       end.sort
     end
 
     def remove_oldest_directory
       directory_to_remove = all_directories.shift
-      path_to_remove = File.join(action_directory, directory_to_remove)
+      path_to_remove = File.join(base_directory_path, directory_to_remove)
       FileUtils.rm_rf(path_to_remove)
     end
 
-    def action_directory
-      @action_directory ||= begin
-        controller_name = Thread.current[:current_controller].underscore
-        action_name = Thread.current[:current_action]
-        File.join(Rails.root, "log", "custom_log_space", controller_name, action_name)
-      end
+    def base_directory_path
+      File.join(Rails.root, "log", "custom_log_space", current_controller.underscore, current_action)
     end
 
     def write_to_custom_log(message)
-      directory_path = log_directory_based_on_format
+      directory_path = File.join(base_directory_path, Time.now.strftime("%Y-%m-%d"))
       FileUtils.mkdir_p(directory_path) unless Dir.exist?(directory_path)
-      custom_log_path = custom_log_file_path(directory_path)
+      custom_log_path = "#{directory_path}/#{Time.now.strftime("%H:%M")}.log"
 
       File.open(custom_log_path, "a") do |file|
         yield(file) # Header or other info can be passed and written here
         file.puts(message)
       end
     rescue SystemCallError, IOError => e
-      handle_file_error(e)
+      error_prefix = e.is_a?(SystemCallError) ? "Error" : "IO Error"
+      puts "#{error_prefix}: #{e.message}"
     end
 
-    def handle_file_error(error)
-      error_prefix = error.is_a?(SystemCallError) ? "Error" : "IO Error"
-      puts "#{error_prefix}: #{error.message}"
-    end
-
+    # rubocop:disable Metrics/AbcSize
     def write_header_information(file)
       return if Thread.current[:header_written]
 
-      current_controller = Thread.current[:current_controller]
-      current_action = Thread.current[:current_action]
-
       file.puts("") # Add a blank line for better readability.
-      write_request_info(file)
-      write_processing_info(file, current_controller, current_action)
-      write_parameters_info(file)
-      Thread.current[:header_written] = true
-    end
-
-    def write_request_info(file)
-      formatted_time = Time.now.strftime("%Y-%m-%d %H:%M:%S %z")
-      file.puts "Started GET \"#{Thread.current[:path]}\" for ::1 at #{formatted_time}"
-    end
-
-    def write_processing_info(file, current_controller, current_action)
+      file.puts "Started GET \"#{Thread.current[:path]}\" for ::1 at #{Time.now.strftime("%Y-%m-%d %H:%M:%S %z")}"
       file.puts "Processing by #{current_controller}##{current_action} as HTML"
-    end
 
-    def write_parameters_info(file)
       params = Thread.current[:params] || {}
       file.puts "Parameters: #{params.inspect}" unless params.empty?
-    end
 
-    def custom_log_file_path(directory_path)
-      time = Time.now.strftime("%H:%M")
-      "#{directory_path}/#{time}.log"
+      Thread.current[:header_written] = true
     end
-
-    def log_directory_based_on_format
-      controller_name = Thread.current[:current_controller].underscore
-      action_name = Thread.current[:current_action]
-      date = Time.now.strftime("%Y-%m-%d")
-
-      File.join(Rails.root, "log", "custom_log_space", controller_name, action_name, date)
-    end
+    # rubocop:enable Metrics/AbcSize
   end
 end
